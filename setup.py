976666254386ccda1f428b0bc1ebf8738ee4d25f
Kieran Meteyard <kmeteyard@mango-solution.com>
2020-02-08 11:37:01 +0000

    basic package structure added

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='imdb_nlp',
+      version='0.1',
+      description='Model movie reviews',
+      url='https://github.com/kmeteyard/imdb_nlp',
+      author='Kieran Meteyard',
+      author_email='kieran.meteyard@gmail.com',
+      license='GNU',
+      packages=['imdb_nlp'],
+      include_package_data=True,
+      zip_safe=False)
